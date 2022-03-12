00e040d2d1ad3d004db6f9e546791ce4f8239774
Андрей Скоробогатов <ufo1og@yandex.ru>
2022-03-12 17:27:30 +0500

    First version of RomanConverter

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         RomanConverter romanConverter = new RomanConverter();
 
+        String romanNumber = "LD";
+        int arabicNumber = romanConverter.convertRomanToArabic(romanNumber);
+        if (arabicNumber < 0) System.out.println("Некорректное римское число!");
+        else System.out.println(romanNumber + " по арабски это " + arabicNumber);
     }
 }
--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class RomanConverter {
-    private HashMap<String, Integer> romanMap = new HashMap<>();
+    private final HashMap<String, Integer> romanMap = new HashMap<>();
 
     public RomanConverter() {
         this.romanMap.put("I", 1);
@@ -19,17 +19,48 @@ public class RomanConverter {
         return true;
     }
 
-    private boolean isCorrectSequence (String[] romanNumbers) {
-        String previous = romanNumbers[0];
-        for (int i = 1; i < romanNumbers.length; i++) {
-            //TODO
+    private int[] splitToArabicNumbers (String romanNumber) {
+        String[] romanNumbers = romanNumber.split("");
+        if (isCorrectSymbols(romanNumbers)) {
+            int[] numbers = new int[romanNumbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = romanMap.get(romanNumbers[i]);
+            }
+            return numbers;
+        }
+        return null;
+    }
+
+    private boolean checkOverStreak (int[] numbers, int index) {
+        if (index + 2 >= numbers.length) return true;
+        return numbers[index] != numbers[index + 1] || numbers[index] != numbers[index + 2];
+    }
+
+    private boolean isCorrectSequence (int[] numbers) {
+        if (numbers == null) return false;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i - 1] == numbers[i]) {
+                if (numbers[i - 1] == numbers[i]) {
+                    if (numbers[i] == romanMap.get("V") || numbers[i] == romanMap.get("L") || numbers[i] == romanMap.get("D")) return false;
+                    if (numbers[i] == romanMap.get("I") || numbers[i] == romanMap.get("X") || numbers[i] == romanMap.get("C")) return checkOverStreak(numbers, i);
+                }
+            }
+            if (numbers[i - 1] * 10 < numbers[i]) return false;
+            else if (numbers[i - 1] * 10 == numbers[i] && (numbers[i - 1] == romanMap.get("L") || numbers[i - 1] == romanMap.get("V"))) return false;
+            if (numbers[i - 1] == numbers[i] / 2) return false;
         }
         return true;
     }
 
-    private String[] splitRomanNumber (String romanNumber) {
-        String[] romanNumbers = romanNumber.split("");
-        //TODO
-        return null;
+    public int convertRomanToArabic (String romanNumber) {
+        int[] numbers = splitToArabicNumbers(romanNumber);
+        if (!isCorrectSequence(numbers)) return -1;
+        int arabicNumber = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] < numbers[i + 1]) arabicNumber -= numbers[i];
+            else arabicNumber += numbers[i];
+        }
+        arabicNumber += numbers[numbers.length - 1];
+        return arabicNumber;
     }
 }
